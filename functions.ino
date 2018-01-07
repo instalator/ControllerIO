6a5db13593b4e2012b9d9530c3073dd559fe56cc
Vladimir Vilisov <vvvalt@mail.ru>
2018-01-07 21:32:28 +0700

    Update functions.ino

--- functions.ino
+++ functions.ino
@@ -1,57 +1,77 @@
 void callback_iobroker(String strTopic, String strPayload) {
   if (strTopic == "myhome/controller/Reset") {
-      if(strPayload == "true"){
-        client.publish("myhome/controller/Reset", "false");
-        wdt_enable(WDTO_15MS);
-        for(;;){}
-      }
+    if (strPayload == "true") {
+      client.publish("myhome/controller/Reset", "false");
+      wdt_enable(WDTO_15MS);
+      for (;;) {}
+    }
   }
   //////////////////////////
   else if (strTopic == "myhome/controller/PWM_1") {
-      int pwm = PWM(strPayload);
-      analogWrite(PWM_1, pwm);
-      client.publish("myhome/controller/PWM_1", IntToChar(255 - pwm));
+    int pwm = PWM(strPayload);
+    analogWrite(PWM_1, pwm);
+    client.publish("myhome/controller/PWM_1", IntToChar(255 - pwm));
   }
   //////////////////////////
   else if (strTopic == "myhome/controller/PWM_2") {
-      int pwm = PWM(strPayload);
-      analogWrite(PWM_2, pwm);
-      client.publish("myhome/controller/PWM_2", IntToChar(255 - pwm));
+    int pwm = PWM(strPayload);
+    analogWrite(PWM_2, pwm);
+    client.publish("myhome/controller/PWM_2", IntToChar(255 - pwm));
   }
   //////////////////////////
   else if (strTopic == "myhome/controller/SSR_1") {
-      digitalWrite(SSR_1, SrtToLvl(strPayload));
-      client.publish("myhome/controller/SSR_1", pub(strPayload));
+    digitalWrite(SSR_1, SrtToLvl(strPayload));
+    client.publish("myhome/controller/SSR_1", pub(strPayload));
   }
   //////////////////////////
   else if (strTopic == "myhome/controller/SSR_2") {
-      digitalWrite(SSR_2, SrtToLvl(strPayload));
-      client.publish("myhome/controller/SSR_2", pub(strPayload));
+    digitalWrite(SSR_2, SrtToLvl(strPayload));
+    client.publish("myhome/controller/SSR_2", pub(strPayload));
   }
   //////////////////////////
   else if (strTopic == "myhome/controller/RELAY_1") {
-      digitalWrite(RELAY_1, SrtToLvl(strPayload));
-      client.publish("myhome/controller/RELAY_1", pub(strPayload));
+    digitalWrite(RELAY_1, SrtToLvl(strPayload));
+    client.publish("myhome/controller/RELAY_1", pub(strPayload));
   }
   //////////////////////////
   else if (strTopic == "myhome/controller/RELAY_2") {
-      digitalWrite(RELAY_2, SrtToLvl(strPayload));
-      client.publish("myhome/controller/RELAY_2", pub(strPayload));
+    digitalWrite(RELAY_2, SrtToLvl(strPayload));
+    client.publish("myhome/controller/RELAY_2", pub(strPayload));
   }
   //////////////////////////
 }
 
-void AnalogRead(){
+void AnalogRead() {
   unsigned int Ain_1 = analogRead(AIN_1);
   unsigned int Ain_2 = analogRead(AIN_2);
   unsigned int Ain_3 = analogRead (AIN_3);
   unsigned int Ain_4 = analogRead (AIN_4);
+  if (Ain_1 == 1 || (Ain_1 == 2 && old_Ain_1 == 0)) {
+    Ain_1 = 0;
+  }
+  if (Ain_2 == 1 || (Ain_2 == 2 && old_Ain_2 == 0)) {
+    Ain_2 = 0;
+  }
+  if (Ain_3 == 1 || (Ain_3 == 2 && old_Ain_3 == 0)) {
+    Ain_3 = 0;
+  }
+  if (Ain_4 == 1 || (Ain_4 == 2 && old_Ain_4 == 0)) {
+    Ain_4 = 0;
+  }
+  if (Ain_1 - 2 == old_Ain_1) {
+    Ain_1 = old_Ain_1;
+  }
+  if (Ain_2 - 2 == old_Ain_2) {
+    Ain_2 = old_Ain_2;
+  }
+  if (Ain_3 - 2 == old_Ain_3) {
+    Ain_3 = old_Ain_3;
+  }
+  if (Ain_4 - 2 == old_Ain_4) {
+    Ain_4 = old_Ain_4;
+  }
 
-  if(Ain_1 == 1){Ain_1 = 0;}
-  if(Ain_2 == 1){Ain_2 = 0;}
-  if(Ain_3 == 1){Ain_3 = 0;}
-  if(Ain_4 == 1){Ain_4 = 0;}
-  if (((old_Ain_1 != Ain_1 || old_Ain_2 != Ain_2 || old_Ain_3 != Ain_3 || old_Ain_4 != Ain_4) && millis() - prevMillis2 > 1000) || firststart){
+  if (((old_Ain_1 != Ain_1 || old_Ain_2 != Ain_2 || old_Ain_3 != Ain_3 || old_Ain_4 != Ain_4) && millis() - prevMillis2 > 1000) || firststart) {
     prevMillis2 = millis();
     old_Ain_1 = Ain_1;
     old_Ain_2 = Ain_2;
@@ -62,12 +82,13 @@ void AnalogRead(){
     client.publish("myhome/controller/AIN_3", IntToChar(Ain_3));
     client.publish("myhome/controller/AIN_4", IntToChar(Ain_4));
   }
+  firststart = false;
 }
 
-void DinRead(){
+void DinRead() {
   int Din_1 = digitalRead(DIN_1);
   int Din_2 = digitalRead(DIN_2);
-  if (((old_din_1 != Din_1 || old_din_2 != Din_2) && millis() - prevMillis3 > 500) || firststart){
+  if (((old_din_1 != Din_1 || old_din_2 != Din_2) && millis() - prevMillis3 > 500) || firststart) {
     prevMillis3 = millis();
     old_din_1 = Din_1;
     old_din_2 = Din_2;
@@ -77,107 +98,126 @@ void DinRead(){
   }
 }
 
+void ReadDS18() {
+  DS_sensors.requestTemperatures();
+  float Temp1 = DS_sensors.getTempC(addr_T1);
+  client.publish("myhome/controller/Temperature_1", FloatToChar(Temp1));
+}
 
-void ReadButton (){
-  if (firststart){
-    for(int i = 0; i <= 15; i++){
+void ReadDHT() {
+  float Hout = 0;
+  float Tout = 0;
+  if (DHT.read22(DHT22_PIN) == DHTLIB_OK) {
+    Hout = DHT.humidity;
+    Tout = DHT.temperature;
+  }
+  if (Hout != 0 || Tout != 0) {
+    client.publish("myhome/controller/DHT_humidity", FloatToChar(Hout));
+    client.publish("myhome/controller/DHT_temperature", FloatToChar(Tout));
+  }
+
+}
+
+void ReadButton () {
+  if (firststart) {
+    for (int i = 0; i <= 15; i++) {
       btn_old[i] = mcp.digitalRead(bt[i]);
       delay(100);
       Switch(i);
     }
   } else {
-    for(int i = 0; i <= 15; i++){
+    for (int i = 0; i <= 15; i++) {
       btn[i] = mcp.digitalRead(bt[i]);
-      if (btn[i] != btn_old[i]){
+      if (btn[i] != btn_old[i]) {
         //if (millis() - prevMillisPoll > 100){
-          //prevMillisPoll = millis();
-          btn_old[i] = btn[i];
-          Switch(i);
-       //}
+        //prevMillisPoll = millis();
+        btn_old[i] = btn[i];
+        Switch(i);
+        //}
       }
     }
   }
 }
 
-void Switch(int i){
-  switch (i){
+void Switch(int i) {
+  switch (i) {
     case 0:
-        client.publish("myhome/controller/IN_1", state(btn_old[i]));
+      client.publish("myhome/controller/IN_1", state(btn_old[i]));
       break;
     case 1:
-        client.publish("myhome/controller/IN_2", state(btn_old[i]));
+      client.publish("myhome/controller/IN_2", state(btn_old[i]));
       break;
     case 2:
-        client.publish("myhome/controller/IN_3", state(btn_old[i]));
+      client.publish("myhome/controller/IN_3", state(btn_old[i]));
       break;
     case 3:
-        client.publish("myhome/controller/IN_4", state(btn_old[i]));
+      client.publish("myhome/controller/IN_4", state(btn_old[i]));
       break;
     case 4:
-        client.publish("myhome/controller/IN_5", state(btn_old[i]));
+      client.publish("myhome/controller/IN_5", state(btn_old[i]));
       break;
     case 5:
-        client.publish("myhome/controller/IN_6", state(btn_old[i]));
+      client.publish("myhome/controller/IN_6", state(btn_old[i]));
       break;
     case 6:
-        client.publish("myhome/controller/IN_7", state(btn_old[i]));
+      client.publish("myhome/controller/IN_7", state(btn_old[i]));
       break;
     case 7:
-        client.publish("myhome/controller/IN_8", state(btn_old[i]));
+      client.publish("myhome/controller/IN_8", state(btn_old[i]));
       break;
     case 8:
-        client.publish("myhome/controller/IN_9", state(btn_old[i]));
+      client.publish("myhome/controller/IN_9", state(btn_old[i]));
       break;
     case 9:
-        client.publish("myhome/controller/IN_10", state(btn_old[i]));
+      client.publish("myhome/controller/IN_10", state(btn_old[i]));
       break;
     case 10:
-        client.publish("myhome/controller/IN_11", state(btn_old[i]));
+      client.publish("myhome/controller/IN_11", state(btn_old[i]));
       break;
     case 11:
-        client.publish("myhome/controller/IN_12", state(btn_old[i]));
+      client.publish("myhome/controller/IN_12", state(btn_old[i]));
       break;
     case 12:
-        client.publish("myhome/controller/IN_13", state(btn_old[i]));
+      client.publish("myhome/controller/IN_13", state(btn_old[i]));
       break;
     case 13:
-        client.publish("myhome/controller/IN_14", state(btn_old[i]));
+      client.publish("myhome/controller/IN_14", state(btn_old[i]));
       break;
     case 14:
-        client.publish("myhome/controller/IN_15", state(btn_old[i]));
+      client.publish("myhome/controller/IN_15", state(btn_old[i]));
       break;
     case 15:
-        client.publish("myhome/controller/IN_16", state(btn_old[i]));
+      client.publish("myhome/controller/IN_16", state(btn_old[i]));
       break;
-    //default:
+      //default:
   }
 }
 
-const char* state(int num){
-    if (num > 0){
-      return "false";
-    } else {
-      return "true";
-    }
+const char* state(int num) {
+  if (num > 0) {
+    return "false";
+  } else {
+    return "true";
+  }
 }
 
-bool SrtToLvl(String st){
-    if (st == "false" || st == "0" || st == "off"){
-      return 0;
-    } else {
-      return 1;
-    }
+bool SrtToLvl(String st) {
+  if (st == "false" || st == "0" || st == "off") {
+    return 0;
+  } else {
+    return 1;
+  }
 }
-const char* pub(String st){
-    if (st == "false" || st == "0" || st == "off"){
-      return "false";
-    } else{
-      return "true";
-    }
+const char* pub(String st) {
+  if (st == "false" || st == "0" || st == "off") {
+    return "false";
+  } else {
+    return "true";
+  }
 }
 
 const char* BoolToChar (bool r) {
-    return r ? "true" : "false";
+  return r ? "true" : "false";
 }
 
 const char* IntToChar (unsigned int v) {
@@ -185,13 +225,18 @@ const char* IntToChar (unsigned int v) {
   return buf;
 }
 
-int PWM(String p){
-    pwm = p.toInt();
-    if (pwm > 254){
-      pwm = 255;
-    } else if (pwm < 1){
-      pwm = 0;
-    }
-    pwm = 255 - pwm;
-    return pwm;
+const char* FloatToChar (float f) {
+  sprintf(buf, "%d.%02d", (int)f, (int)(f * 100) % 100);
+  return buf;
+}
+
+int PWM(String p) {
+  pwm = p.toInt();
+  if (pwm > 254) {
+    pwm = 255;
+  } else if (pwm < 1) {
+    pwm = 0;
+  }
+  pwm = 255 - pwm;
+  return pwm;
 }
