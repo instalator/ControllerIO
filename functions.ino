bd07937a2f64fd212519f06488cce0ee1b9a53e0
Vladimir Vilisov <vvvalt@mail.ru>
2018-10-07 19:52:42 +0700

    Update functions.ino

--- functions.ino
+++ functions.ino
@@ -1,121 +1,122 @@
-void callback_iobroker(String strTopic, String strPayload) {
-  if (strTopic == "myhome/controller/Reset") {
-    if (strPayload == "true") {
-      client.publish("myhome/controller/Reset", "false");
+void callback_iobroker(String topic, String payload) {
+  if (topic.equals("myhome/controller/Reset")) {
+    if (payload == "true") {
+      mqtt.publish("myhome/controller/Reset", "false");
       wdt_enable(WDTO_15MS);
       for (;;) {}
     }
   }
   //////////////////////////
-  else if (strTopic == "myhome/controller/PWM_1") {
-    int pwm = PWM(strPayload);
+  else if (topic.equals("myhome/controller/PWM_1")) {
+    int pwm = PWM(payload);
     analogWrite(PWM_1, pwm);
-    client.publish("myhome/controller/PWM_1", IntToChar(255 - pwm));
+    mqtt.publish("myhome/controller/PWM_1", IntToChar(255 - pwm));
   }
   //////////////////////////
-  else if (strTopic == "myhome/controller/PWM_2") {
-    int pwm = PWM(strPayload);
+  else if (topic.equals("myhome/controller/PWM_2")) {
+    int pwm = PWM(payload);
     analogWrite(PWM_2, pwm);
-    client.publish("myhome/controller/PWM_2", IntToChar(255 - pwm));
+    mqtt.publish("myhome/controller/PWM_2", IntToChar(255 - pwm));
   }
   //////////////////////////
-  else if (strTopic == "myhome/controller/SSR_1") {
-    digitalWrite(SSR_1, SrtToLvl(strPayload));
-    client.publish("myhome/controller/SSR_1", pub(strPayload));
+  else if (topic.equals("myhome/controller/SSR_1")) {
+    digitalWrite(SSR_1, SrtToLvl(payload));
+    mqtt.publish("myhome/controller/SSR_1", pub(payload));
   }
   //////////////////////////
-  else if (strTopic == "myhome/controller/SSR_2") {
-    digitalWrite(SSR_2, SrtToLvl(strPayload));
-    client.publish("myhome/controller/SSR_2", pub(strPayload));
+  else if (topic.equals("myhome/controller/SSR_2")) {
+    digitalWrite(SSR_2, SrtToLvl(payload));
+    mqtt.publish("myhome/controller/SSR_2", pub(payload));
   }
   //////////////////////////
-  else if (strTopic == "myhome/controller/RELAY_1") {
-    digitalWrite(RELAY_1, SrtToLvl(strPayload));
-    client.publish("myhome/controller/RELAY_1", pub(strPayload));
+  else if (topic.equals("myhome/controller/RELAY_1")) {
+    digitalWrite(RELAY_1, SrtToLvl(payload));
+    mqtt.publish("myhome/controller/RELAY_1", pub(payload));
   }
   //////////////////////////
-  else if (strTopic == "myhome/controller/RELAY_2") {
-    digitalWrite(RELAY_2, SrtToLvl(strPayload));
-    client.publish("myhome/controller/RELAY_2", pub(strPayload));
+  else if (topic.equals("myhome/controller/RELAY_2")) {
+    digitalWrite(RELAY_2, SrtToLvl(payload));
+    mqtt.publish("myhome/controller/RELAY_2", pub(payload));
   }
   //////////////////////////
 }
 
-void AnalogRead() {
-  unsigned int Ain_1 = analogRead(AIN_1);
-  unsigned int Ain_2 = analogRead(AIN_2);
-  unsigned int Ain_3 = analogRead (AIN_3);
-  unsigned int Ain_4 = analogRead (AIN_4);
-  if (Ain_1 == 1 || (Ain_1 == 2 && old_Ain_1 == 0)) {
-    Ain_1 = 0;
+void AnalogRead() { //http://arduino.ru/forum/programmirovanie/usrednenie-znachenii-s-potentsiometra
+  int Ain[4] = {analogRead(AIN_1), analogRead(AIN_2), analogRead (AIN_3), analogRead (AIN_4)};
+  if (Ain[0] == 1 || (Ain[0] == 2 && old_Ain[0] == 0)) {
+    Ain[0] = 0;
   }
-  if (Ain_2 == 1 || (Ain_2 == 2 && old_Ain_2 == 0)) {
-    Ain_2 = 0;
+  if (Ain[1] == 1 || (Ain[1] == 2 && old_Ain[1] == 0)) {
+    Ain[1] = 0;
   }
-  if (Ain_3 == 1 || (Ain_3 == 2 && old_Ain_3 == 0)) {
-    Ain_3 = 0;
+  if (Ain[2] == 1 || (Ain[2] == 2 && old_Ain[2] == 0)) {
+    Ain[2] = 0;
   }
-  if (Ain_4 == 1 || (Ain_4 == 2 && old_Ain_4 == 0)) {
-    Ain_4 = 0;
+  if (Ain[3] == 1 || (Ain[3] == 2 && old_Ain[3] == 0)) {
+    Ain[3] = 0;
   }
-  if (Ain_1 - 2 == old_Ain_1) {
-    Ain_1 = old_Ain_1;
+  if (Ain[0] - 2 == old_Ain[0]) {
+    Ain[0] = old_Ain[0];
   }
-  if (Ain_2 - 2 == old_Ain_2) {
-    Ain_2 = old_Ain_2;
+  if (Ain[1] - 2 == old_Ain[1]) {
+    Ain[1] = old_Ain[1];
   }
-  if (Ain_3 - 2 == old_Ain_3) {
-    Ain_3 = old_Ain_3;
+  if (Ain[2] - 2 == old_Ain[2]) {
+    Ain[2] = old_Ain[2];
   }
-  if (Ain_4 - 2 == old_Ain_4) {
-    Ain_4 = old_Ain_4;
+  if (Ain[3] - 2 == old_Ain[3]) {
+    Ain[4] = old_Ain[3];
   }
 
-  if (((old_Ain_1 != Ain_1 || old_Ain_2 != Ain_2 || old_Ain_3 != Ain_3 || old_Ain_4 != Ain_4) && millis() - prevMillis2 > 1000) || firststart) {
+  if (((old_Ain[0] != Ain[0] || old_Ain[1] != Ain[1] || old_Ain[2] != Ain[2] || old_Ain[3] != Ain[3]) && millis() - prevMillis2 > 1000) || firststart) {
     prevMillis2 = millis();
-    old_Ain_1 = Ain_1;
-    old_Ain_2 = Ain_2;
-    old_Ain_3 = Ain_3;
-    old_Ain_4 = Ain_4;
-    client.publish("myhome/controller/AIN_1", IntToChar(Ain_1));
-    client.publish("myhome/controller/AIN_2", IntToChar(Ain_2));
-    client.publish("myhome/controller/AIN_3", IntToChar(Ain_3));
-    client.publish("myhome/controller/AIN_4", IntToChar(Ain_4));
+    old_Ain[0] = Ain[0];
+    old_Ain[1] = Ain[1];
+    old_Ain[2] = Ain[2];
+    old_Ain[3] = Ain[3];
+    for (int i = 0; i < 4; i++) {
+      String t = "myhome/controller/AIN_";
+      t += i + 1;
+      const char *q = t.c_str();
+      mqtt.publish(q, IntToChar(Ain[i]));
+    }
   }
+  
   firststart = false;
 }
 
-void DinRead() {
+/*
+  void DinRead() {
   int Din_1 = digitalRead(DIN_1);
   int Din_2 = digitalRead(DIN_2);
   if (((old_din_1 != Din_1 || old_din_2 != Din_2) && millis() - prevMillis3 > 500) || firststart) {
     prevMillis3 = millis();
     old_din_1 = Din_1;
     old_din_2 = Din_2;
-    client.publish("myhome/controller/DIN_1", BoolToChar(Din_1));
-    client.publish("myhome/controller/DIN_2", BoolToChar(Din_2));
+    mqtt.publish("myhome/controller/DIN_1", BoolToChar(Din_1));
+    mqtt.publish("myhome/controller/DIN_2", BoolToChar(Din_2));
     firststart = false;
   }
-}
+  }
+*/
 
 void ReadDS18() {
   DS_sensors.requestTemperatures();
-  float Temp1 = DS_sensors.getTempC(addr_T1);
-  client.publish("myhome/controller/Temperature_1", FloatToChar(Temp1));
+  Temp1 = DS_sensors.getTempC(addr_T1);
+  if (Temp1 > 0) {
+    mqtt.publish("myhome/controller/Temperature_1", FloatToChar(Temp1));
+  }
 }
 
 void ReadDHT() {
-  float Hout = 0;
-  float Tout = 0;
   if (DHT.read22(DHT22_PIN) == DHTLIB_OK) {
     Hout = DHT.humidity;
     Tout = DHT.temperature;
+    if (Hout != 0 || Tout != 0) {
+      mqtt.publish("myhome/controller/DHT_humidity", FloatToChar(Hout));
+      mqtt.publish("myhome/controller/DHT_temperature", FloatToChar(Tout));
+    }
   }
-  if (Hout != 0 || Tout != 0) {
-    client.publish("myhome/controller/DHT_humidity", FloatToChar(Hout));
-    client.publish("myhome/controller/DHT_temperature", FloatToChar(Tout));
-  }
-
 }
 
 void ReadButton () {
@@ -140,57 +141,10 @@ void ReadButton () {
 }
 
 void Switch(int i) {
-  switch (i) {
-    case 0:
-      client.publish("myhome/controller/IN_1", state(btn_old[i]));
-      break;
-    case 1:
-      client.publish("myhome/controller/IN_2", state(btn_old[i]));
-      break;
-    case 2:
-      client.publish("myhome/controller/IN_3", state(btn_old[i]));
-      break;
-    case 3:
-      client.publish("myhome/controller/IN_4", state(btn_old[i]));
-      break;
-    case 4:
-      client.publish("myhome/controller/IN_5", state(btn_old[i]));
-      break;
-    case 5:
-      client.publish("myhome/controller/IN_6", state(btn_old[i]));
-      break;
-    case 6:
-      client.publish("myhome/controller/IN_7", state(btn_old[i]));
-      break;
-    case 7:
-      client.publish("myhome/controller/IN_8", state(btn_old[i]));
-      break;
-    case 8:
-      client.publish("myhome/controller/IN_9", state(btn_old[i]));
-      break;
-    case 9:
-      client.publish("myhome/controller/IN_10", state(btn_old[i]));
-      break;
-    case 10:
-      client.publish("myhome/controller/IN_11", state(btn_old[i]));
-      break;
-    case 11:
-      client.publish("myhome/controller/IN_12", state(btn_old[i]));
-      break;
-    case 12:
-      client.publish("myhome/controller/IN_13", state(btn_old[i]));
-      break;
-    case 13:
-      client.publish("myhome/controller/IN_14", state(btn_old[i]));
-      break;
-    case 14:
-      client.publish("myhome/controller/IN_15", state(btn_old[i]));
-      break;
-    case 15:
-      client.publish("myhome/controller/IN_16", state(btn_old[i]));
-      break;
-      //default:
-  }
+  String t = "myhome/controller/IN_";
+  t += i + 1;
+  const char *q = t.c_str();
+  mqtt.publish(q, state(btn_old[i]));
 }
 
 const char* state(int num) {
@@ -221,13 +175,13 @@ const char* BoolToChar (bool r) {
 }
 
 const char* IntToChar (unsigned int v) {
-  sprintf(buf, "%d", v);
-  return buf;
+  sprintf(buffer, "%d", v);
+  return buffer;
 }
 
 const char* FloatToChar (float f) {
-  sprintf(buf, "%d.%02d", (int)f, (int)(f * 100) % 100);
-  return buf;
+  sprintf(buffer, "%d.%02d", (int)f, (int)(f * 100) % 100);
+  return buffer;
 }
 
 int PWM(String p) {
